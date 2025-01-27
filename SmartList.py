2585beaf13caf947ef5d119cc8e722f4ec689bdb
ibrahim-alatyan <ibrooomcraft@gmail.com>
2025-01-27 22:51:37 +0300

    vdf

--- SmartList.py
+++ SmartList.py
@@ -5,16 +5,34 @@ st.title("SmartList")
 "\n"
 "\n"
 
+def add_big_task(title):
+    with Rcont:
+        with st.expander(title):
+            st.checkbox(title)
 LCol, RCol = st.columns(2)
 
 with RCol:
-    st.header("Tasks")
-    st.button("Add Task")
+    Rcont = st.container()
+    with Rcont:
+        Rcont.header("Tasks")
+        task_textbox = st.text_input("Enter task: " )
+        if st.button("Add task"):
+            add_big_task(task_textbox)
 
+   
+    
 with LCol:
-    st.header("Ai Assessmint")
+    Lcont = st.container()
+    with Lcont:
+        Lcont.header("Ai Assessmint")
 
 "\n"
 "\n"
 "\n"
 st.write("out side line")
+
+    # task_textbox = st.text_input("Enter big task: ")
+    # if st.button("Add Big task"):
+    #     if task_textbox:
+    #         st.session_state["TaskList"] = []
+         
