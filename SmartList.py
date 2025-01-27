41177eed8f05743872f7289064c8f53d20f7a2ee
ibrahim-alatyan <ibrooomcraft@gmail.com>
2025-01-27 21:46:42 +0300

    ANA

--- SmartList.py
+++ SmartList.py
@@ -1,2 +1,20 @@
-import math as m
+import streamlit as st
 
+st.title("SmartList")
+"\n"
+"\n"
+"\n"
+
+LCol, RCol = st.columns(2)
+
+with RCol:
+    st.header("Tasks")
+    st.button("Add Task")
+
+with LCol:
+    st.header("Ai Assessmint")
+
+"\n"
+"\n"
+"\n"
+st.write("out side line")
