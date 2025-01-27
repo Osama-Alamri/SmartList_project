de85639d898190ab78b71695c2ea0ccdc8cc2cbe
ibrahim-alatyan <ibrooomcraft@gmail.com>
2025-01-28 01:20:17 +0300

    wefe

--- SmartList.py
+++ SmartList.py
@@ -2,8 +2,6 @@ import streamlit as st
 
 st.title("SmartList")
 "\n"
-"\n"
-"\n"
 
 if "task_list" not in st.session_state:
     st.session_state["task_list"] = []
@@ -28,36 +26,66 @@ def add_task(task_title):
     else:
         st.warning("Please enter a task before adding.")
 
-LCol, RCol = st.columns(2)
+LCol , MCol , RCol = st.columns([3,1,2])
+
+with LCol:
+    st.title("ChatBot")
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+
+    chat_container = st.container(border=True, height= 600)
+
+    with chat_container:
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+
+    prompt = st.chat_input("enter a message")
+    if prompt:
+        st.chat_message("user").markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        
+        response = f"Echo: {prompt}"
+        with st.chat_message("assistant"):
+            st.markdown(response)
+
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+with MCol:
+    st.title(" ")
 
 with RCol:
     st.title("To-Do List")
-    task_textbox = st.text_input("Enter task: " )
-    if st.button("Add task"):
-        add_task(task_textbox)
-    st.write("Tasks: ")
 
-    #expander
-    for task in st.session_state["task_list"]:
-        with st.expander(task):
-            sup_task_textbox = st.text_input(f"Enter a sub-task for {task}")
-            if st.button(f"Add Sup-task to {task}"):
-                add_sup_task(sup_task_textbox, task)
-        #sup-task down the expander
-            st.checkbox(task)
-            if task in st.session_state["suptask_list"]:
-                for sup_task in st.session_state["suptask_list"][task]:
-                    st.checkbox(sup_task)
+    toDo_container = st.container(border=False, height = 900)
+    with toDo_container:
+        task_textbox = st.text_input("Enter task: " )
+        if st.button("Add task"):
+            add_task(task_textbox)
+        st.write("Tasks: ")
+
+        #expander
+        for task in st.session_state["task_list"]:
+            with st.expander(task):
+                sup_task_textbox = st.text_input(f"Enter a sub-task for {task}")
+                if st.button(f"Add Sup-task to {task}"):
+                    add_sup_task(sup_task_textbox, task)
+            #sup-task down the expander
+                st.checkbox(task)
+                if task in st.session_state["suptask_list"]:
+                    for sup_task in st.session_state["suptask_list"][task]:
+                        st.checkbox(sup_task)
 
     
-with LCol:
-    Lcont = st.container()
-    with Lcont:
-        Lcont.title("Ai Assessmint")
 
-"\n"
-"\n"
-"\n"
-st.write("out side 1")
+
+# "\n"
+# "\n"
+# "\n"
+# st.write("out side 1")
 
          
--- test.py
+++ test.py
@@ -2,27 +2,8 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
+
 with col1:
-    st.title("To-Do List")
-
-    if "task_list" not in st.session_state:
-        st.session_state["task_list"] = []
-
-    task = st.text_input("Enter your task", "")
-
-    if st.button("Add the task"):
-        if task:
-            st.session_state["task_list"].append(task)
-            st.success("Task added!")
-        else:
-            st.warning("Please enter a task before adding.")
-
-    for i , t in enumerate(st.session_state["task_list"]):
-        if st.checkbox(f"{i+1}. {t}"):
-            st.session_state["task_list"].remove(t)
-        
-
-with col2:
     st.title("ChatBot")
 
     if "messages" not in st.session_state:
@@ -48,3 +29,23 @@ with col2:
             st.markdown(response)
 
         st.session_state.messages.append({"role": "assistant", "content": response})
+
+with col2:
+    st.title("To-Do List")
+
+    if "task_list" not in st.session_state:
+        st.session_state["task_list"] = []
+
+    task = st.text_input("Enter your task", "")
+
+    if st.button("Add the task"):
+        if task:
+            st.session_state["task_list"].append(task)
+            st.success("Task added!")
+        else:
+            st.warning("Please enter a task before adding.")
+
+    for i , t in enumerate(st.session_state["task_list"]):
+        if st.checkbox(f"{i+1}. {t}"):
+            st.session_state["task_list"].remove(t)
+        
